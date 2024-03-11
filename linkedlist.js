8ffbe0c9093d4d09d5c92d7d309793a08369bf30
bkhazaal <thajukez101@gmail.com>
2024-03-11 16:31:55 -0700

    yea

---
+++ linkedlist.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+const node1 = new Node(100);
+console.log(n1);
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+}
+
+insertFirst(data); {
+    this.head = new Node(data, this.head);
+}
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+console.log(ll);
